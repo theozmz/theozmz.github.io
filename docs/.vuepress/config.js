3b2ec520bf7482d5adf7e987c1d49bfd0d1553a8
zmz <2082318160@qq.com>
2025-08-30 23:57:05 +0800

    try

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,4 +9,6 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: '我是不会告诉你们任何事情的啊啊啊啊啊啊啊！',
   description: '喝啊！任何邪恶都将被绳之以法！',
+  base: '/',
+  pagePatterns: ['**/*.md', '!.vuepress', '!node_modules'],
 })
